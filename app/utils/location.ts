6fffef7d675c88f8ff7fa85776425d7d4d1dc1d3
Mr.Hope <mister-hope@outlook.com>
2022-07-18 00:43:58 +0800

    feat: update key

--- app/utils/location.ts
+++ app/utils/location.ts
@@ -4,6 +4,6 @@ const referer = getTitle();
 
 export const navigation = (point: string): void => {
   wx.navigateTo({
-    url: `plugin://routePlan/index?key=NLVBZ-PGJRQ-T7K5F-GQ54N-GIXDH-FCBC4&referer=${referer}&endPoint=${point}&mode=walking&themeColor=#2ecc71`,
+    url: `plugin://routePlan/index?key=7ZXBZ-DZO6W-TK3RO-OGHG5-4J4EQ-PBFFX&referer=${referer}&endPoint=${point}&mode=walking&themeColor=#2ecc71`,
   });
 };
